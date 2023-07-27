996504baec7747a7f95531b764bc9972a036a12c
BananaPilot <fabiopetrosillo2@gmail.com>
2023-07-27 19:23:26 +0200

    adding custom login page  and removed tailwind from project

---
+++ app/signin/page.jsx
@@ -0,0 +1,62 @@
+"use client"
+
+import '@/app/styles/loginpage.css'
+import React, { useState } from 'react'
+import {signIn} from 'next-auth/react'
+
+
+function SingInPage() {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const submitCredentials = () => {
+        signIn( 'credentials', {
+            username,
+            password,
+            callbackUrl: "/"
+        })
+    }
+
+    const submitGoogle = () => {
+        signIn( "google", {
+            callbackUrl: "/"
+        })
+    }
+
+    const submitGitHub = () => {
+        signIn( "github", {
+            callbackUrl: "/"
+        })
+    }
+
+  return (
+    <>
+    <head>
+        <title>
+            | Sign in
+        </title>
+    </head>
+        <div className='container'>
+            <div className='header-section'>
+                <h2>Login:</h2>
+                <p>
+                    By logging in you will have a better expirience in our site, you will be able of placing orders saving items to your favourites and adding items to your cart. 
+                </p>
+            </div>
+            <div className='input-section'>
+                <input onChange={(e) => setUsername(e.target.value)} type="text" placeholder='Enter you username' autoComplete='none'/>
+                <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Enter your password' autoComplete='none'/>
+                <div className='signup-text'>don't have an account <a href="http://localhost:3000/signup">sign up!</a></div>
+                <button onClick={() => {submitCredentials()}} className='signin-button'>Sign in!</button>
+            </div>
+            <div className='division-text'>or</div>
+            <div className='division'></div>
+            <button onClick={() => {submitGoogle()}}>Sign in with Google</button>
+            <button onClick={() => {submitGitHub()}}>Sign in with GitHub</button>
+        </div>
+    </>
+  )
+}
+
+export default SingInPage
