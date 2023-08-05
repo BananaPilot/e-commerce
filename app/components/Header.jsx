914e8ea8c4d6b35ed1724b786e24d028a7d81ec7
BananaPilot <fabiopetrosillo2@gmail.com>
2023-08-05 13:59:33 +0200

    improved code on header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,44 +11,40 @@ function Header() {
     return <img src="https://www.svgrepo.com/show/164688/blank-user.svg" alt="" />
   }
 
-  if (status === "unauthenticated" || status === "loading") {
-    return(
-      <div className='flex justify-between p-3 bg-transparent'>
-        <div className='w-32 p-2'>
-          <img className='cursor-pointer' onClick={() => {location.replace('/')}} src="https://upload.wikimedia.org/wikipedia/commons/c/c8/Spring_%28e-commerce_platform%29_logo.svg"/>
-        </div>
-        <div className='flex'>
-          <button onClick={() => {
-            location.replace('/signin')
-          }} className='text-white bg-violet-700 rounded-xl p-2 hover:bg-violet-500 mx-3'>Sign in</button>
-          <button onClick={() => {
-            location.replace('/favourites')
-          }} className='mx-3'><span className="material-symbols-outlined">&#xe87d;</span></button>
-          <button onClick={() => {
-            location.replace('/cart')
-          }} className='mx-3'><span className="material-symbols-outlined">&#xe8cc;</span></button> 
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className='flex justify-between p-3 bg-transparent'>
         <div className='w-32 p-2'>
           <img className='cursor-pointer' onClick={() => {location.replace('/')}} src="https://upload.wikimedia.org/wikipedia/commons/c/c8/Spring_%28e-commerce_platform%29_logo.svg"/>
         </div>
-        <div className='flex'>
-          <img onError={() => alternative()} className='w-10 rounded-full' src={session?.user.image} alt="" />
-          <button onClick={() => {
-            location.replace('/api/auth/signout')
-          }} className='text-white bg-violet-700 rounded-xl p-2 hover:bg-violet-500 mx-3'>Sign out</button>
-          <button onClick={() => {
-            location.replace('/favourites')
-          }} className='bg-violet-700 hover:bg-violet-500 rounded-full mx-1'><span className="text-white p-2 material-symbols-outlined">&#xe87d;</span></button>
-          <button onClick={() => {
-            location.replace('/cart')
-          }} className='bg-black hover:bg-zinc-900 rounded-full mx-1'><span className="text-white p-2 material-symbols-outlined">&#xe8cc;</span></button> 
-        </div>
+        {
+          status === "unauthenticated" || status === "loading"
+          ?(
+            <div className='flex'>
+              <button onClick={() => {
+                location.replace('/signin')
+              }} className='text-white bg-violet-700 rounded-xl p-2 hover:bg-violet-500 mx-3'>Sign in</button>
+              <button onClick={() => {
+                location.replace('/favourites')
+              }} className='mx-3'><span className="material-symbols-outlined">&#xe87d;</span></button>
+              <button onClick={() => {
+                location.replace('/cart')
+              }} className='mx-3'><span className="material-symbols-outlined">&#xe8cc;</span></button> 
+            </div>
+          ) : (
+            <div className='flex'>
+              <img onError={() => alternative()} className='w-10 rounded-full' src={session?.user.image} alt="" />
+              <button onClick={() => {
+                location.replace('/api/auth/signout')
+              }} className='text-white bg-violet-700 rounded-xl p-2 hover:bg-violet-500 mx-3'>Sign out</button>
+              <button onClick={() => {
+                location.replace('/favourites')
+              }} className='bg-violet-700 hover:bg-violet-500 rounded-full mx-1'><span className="text-white p-2 material-symbols-outlined">&#xe87d;</span></button>
+              <button onClick={() => {
+                location.replace('/cart')
+              }} className='bg-black hover:bg-zinc-900 rounded-full mx-1'><span className="text-white p-2 material-symbols-outlined">&#xe8cc;</span></button> 
+            </div>
+          )
+        }
     </div>
   )
 }
